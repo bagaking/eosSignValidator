7f460bd30736d3090f42b652fa24897acda23b9b
bagaking <zfaustk@qq.com>
2019-04-09 17:39:42 +0800

    feat (router): load router at runtime

--- src/router.ts
+++ src/router.ts
@@ -6,23 +6,24 @@ const objectToArray = (dict: any): any[] => Object.keys(dict).map((name) => dict
 
 const router = new Router();
 
-Object.keys(methods).forEach(name => {
-        router.post("/" + name, async (ctx, next)=>{
-            const method : any = (methods as any)[name];
-            const data = await Promise.resolve(method(ctx.request.body));
-            ctx.status = 200
-            ctx.response.body = {
-                statusCode: 200,
-                result: !! data,
-                data
-            };
-            return await next();
-        })
-        console.log("create api /", name)
-    }
-);
-
 export const useRouters = (app: Koa): Koa => {
+
+    Object.keys(methods).forEach(name => {
+            router.post("/" + name, async (ctx, next)=>{
+                const method : any = (methods as any)[name];
+                const data = await Promise.resolve(method(ctx.request.body));
+                ctx.status = 200
+                ctx.response.body = {
+                    statusCode: 200,
+                    result: !! data,
+                    data
+                };
+                return await next();
+            })
+            console.log("create api /", name)
+        }
+    );
+
     app.use(router.routes());
     app.use(router.allowedMethods());
     return app;
